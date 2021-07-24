afc179ce2d7aaf958fced7f97d10c4836a9b7839
Workstation <o0052ib@gmail.com>
2021-07-25 02:35:33 +0600

    edit ERole

--- src/main/java/kg/abr/backendShopBonus/shopBonus/entity/enums/ERole.java
+++ src/main/java/kg/abr/backendShopBonus/shopBonus/entity/enums/ERole.java
@@ -1,5 +1,6 @@
 package kg.abr.backendShopBonus.shopBonus.entity.enums;
 
 public enum ERole {
-    ROLE_USER, ROLE_MANAGER, ROLE_MODERATOR, ROLE_ADMIN
+    ROLE_USER, ROLE_MANAGER,
+    ROLE_MODERATOR, ROLE_ADMIN
 }
